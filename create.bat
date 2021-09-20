f866a299657ce09ab1a6bf38535207a8df201120
m <m@example.com>
2021-09-20 09:39:34 +0200

    Added build version to builds

--- create.bat
+++ create.bat
@@ -1,4 +1,13 @@
-dotnet build --configuration Release
+@echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=1.%YY%%MM%.%DD%.%HH%%Min%"
+@echo on
+
+
+dotnet build -p:Version=%datestamp% --configuration Release
 
 mkdir installer 2> NUL
 copy ZipBackup\bin\Release\net5.0-windows10.0.17763.0\ZipBackup.exe installer/ZipBackup.exe
